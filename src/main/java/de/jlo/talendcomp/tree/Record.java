e90b62339ef32247fd8b563ef5597b3df5a8645c
Jan Lolling <jan.lolling@gmail.com>
2019-02-27 16:27:42 +0100

    Apache license added Record simplified

--- src/main/java/de/jlo/talendcomp/tree/Record.java
+++ src/main/java/de/jlo/talendcomp/tree/Record.java
@@ -1,12 +1,22 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.jlo.talendcomp.tree;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class Record {
 	
 	private Object key = null;
-	private Map<String, Object> values = null;
+	private Object struct = null;
 	
 	public Record(Object key) {
 		if (key == null) {
@@ -15,19 +25,23 @@ public class Record {
 		this.key = key;
 	}
 	
-	public Object addValue(String key, Object value) {
-		if (values == null) {
-			values = new HashMap<>();
-		}
-		return values.put(key, value);
+	/**
+	 * add here the Talend transfer object (<rowname>Struct)
+	 * @param struct Talend structure object
+	 */
+	public void addStruct(Object struct) {
+		this.struct = struct;
 	}
-	
-	public Object getValue(String key) {
-		return values.get(key);
-	}
-	
+		
+	/**
+	 * Returns the key value
+	 * @return
+	 */
 	public Object getKey() {
 		return key;
 	}
 
+	public Object getStruct() {
+		return struct;
+	}
 }
