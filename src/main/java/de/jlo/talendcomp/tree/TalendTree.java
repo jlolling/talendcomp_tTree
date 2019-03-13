fa412643a383c1bb0251cf5d6b72b4892cd4058e
kai <cornitzius-extern@gvl.de>
2019-03-13 18:19:47 +0100

    TreeInput: Level and Path added

--- src/main/java/de/jlo/talendcomp/tree/TalendTree.java
+++ src/main/java/de/jlo/talendcomp/tree/TalendTree.java
@@ -4,4 +4,14 @@ public class TalendTree extends GenericTree<Record> {
 	public TalendTree() {
 		
 	}
+	
+	public String getPath(Record record) {
+		String ret = "";
+		java.util.List<Record> parents = this.collectParents(record);
+		for (Record parent : parents) {
+			ret = "/" + parent.getKey().toString() + ret;
+		}
+		
+		return ret;
+	}
 }
